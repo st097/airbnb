d0611b33de9a5e331c1e834756a14ed389b7c5f4
st097 <s.tozaj@gmail.com>
2023-09-21 00:13:00 +0200

    react.airbnb

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Hero(){
+    return(
+        <section className="hero">
+          <img src="./images/photo-grid.png" className="hero--photo" />
+        <h1 clasName="hero--header">Online Experiences</h1>
+        <p clasName="hero--text">Join unique interactivities led by one-of-a-kind host-all without leaving home.</p>
+        </section>
+    )
+}
